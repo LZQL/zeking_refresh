2aa3428cedc3c5d9a261d1f48b442310f42ce068
Zeking <396298154@qq.com>
2019-09-11 10:59:49 +0800

    更新一下

---
+++ ios/Classes/ZekingRefreshPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface ZekingRefreshPlugin : NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/ZekingRefreshPlugin.m
@@ -0,0 +1,20 @@
+#import "ZekingRefreshPlugin.h"
+
+@implementation ZekingRefreshPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"zeking_refresh"
+            binaryMessenger:[registrar messenger]];
+  ZekingRefreshPlugin* instance = [[ZekingRefreshPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
